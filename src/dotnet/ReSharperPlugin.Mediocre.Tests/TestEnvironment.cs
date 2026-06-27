bde64b032fe9a3044c5576eadaa4597538906237
Advitalitum <30781212+Advitalitum@users.noreply.github.com>
2026-06-27 23:06:42 +0300

    [MAIN] Initial commit

---
+++ src/dotnet/ReSharperPlugin.Mediocre.Tests/TestEnvironment.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.TestFramework;
+using JetBrains.TestFramework;
+using JetBrains.TestFramework.Application.Zones;
+using NUnit.Framework;
+
+[assembly: Apartment(ApartmentState.STA)]
+
+namespace ReSharperPlugin.Mediocre.Tests
+{
+    [ZoneDefinition]
+    public class MediocreTestEnvironmentZone : ITestsEnvZone, IRequire<PsiFeatureTestZone>, IRequire<IMediocreZone>
+    {
+    }
+
+    [ZoneMarker]
+    public class ZoneMarker : IRequire<ICodeEditingZone>, IRequire<ILanguageCSharpZone>,
+        IRequire<MediocreTestEnvironmentZone>
+    {
+    }
+
+    [SetUpFixture]
+    public class MediocreTestsAssembly : ExtensionTestEnvironmentAssembly<MediocreTestEnvironmentZone>
+    {
+    }
+}
---
+++ src/dotnet/ReSharperPlugin.Mediocre/IMediocreZone.cs
@@ -0,0 +1,10 @@
+using JetBrains.Application.BuildScript.Application.Zones;
+
+namespace ReSharperPlugin.Mediocre
+{
+    [ZoneDefinition]
+    // [ZoneDefinitionConfigurableFeature("Title", "Description", IsInProductSection: false)]
+    public interface IMediocreZone : IZone
+    {
+    }
+}
