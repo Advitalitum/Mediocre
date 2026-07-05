b5f49e6b7165d498db6177a92ff34d0ee98c8e9b
Denis Petukhov <depetukhova@ozon.ru>
2026-07-05 22:10:26 +0300

    [MAIN] Finally

--- src/dotnet/ReSharperPlugin.Mediocre/MediatRCommandHandlerSearcherFactory.cs
+++ src/dotnet/ReSharperPlugin.Mediocre/MediatRCommandHandlerSearcherFactory.cs
@@ -1,24 +1,24 @@
 ﻿using System.Linq;
-using JetBrains.Application.Parts;
+using JetBrains.Application;
+using JetBrains.Application.DataContext;
+using JetBrains.ReSharper.Feature.Services.Navigation.ContextNavigation;
+using JetBrains.ReSharper.Feature.Services.Navigation.Requests;
 using JetBrains.ReSharper.Psi;
-using JetBrains.ReSharper.Psi.CSharp;
-using JetBrains.ReSharper.Psi.ExtensionsAPI;
 using JetBrains.ReSharper.Psi.Search;
 using JetBrains.ReSharper.Psi.Util;
 
 namespace ReSharperPlugin.Mediocre;
 
-[PsiComponent(Instantiation.DemandAnyThreadSafe)]
-public class MediatRCommandHandlerSearcherFactory : DomainSpecificSearcherFactoryBase
+[ShellFeaturePart]
+public class RequestHandlerImplementationContextSearch : ImplementationContextSearch
 {
-    public override bool IsCompatibleWithLanguage(PsiLanguageType languageType)
+    protected override SearchImplementationsRequest CreateSearchRequest(IDataContext context,
+        DeclaredElementTypeUsageInfo element,
+        DeclaredElementTypeUsageInfo initialTarget)
     {
-        return languageType is CSharpLanguage;
-    }
+        var declaredElement = element.DeclaredElement;
 
-    public override NavigateTargets GetNavigateToTargets(IDeclaredElement element)
-    {
-        if (element is IMethod method
+        if (declaredElement is IMethod method
             && method.ShortName == "Send"
             && method.TypeParametersCount == 1
             && method.ContainingType is IInterface @interface
@@ -30,40 +30,130 @@ public class MediatRCommandHandlerSearcherFactory : DomainSpecificSearcherFactor
             && parameterInterface.TypeParametersCount == 1
            )
         {
-            var psiModules = element.GetSolution().GetPsiServices().Modules.GetModules();
+            var psiModule = @interface.Module;
 
-            foreach (var psiModule in psiModules)
+            var scope = psiModule.GetPsiServices().Symbols.GetSymbolScope(
+                psiModule,
+                true,
+                true
+            );
+
+            var requestHandlerTypeElement = scope
+                .GetAllShortNames()
+                .Where(s => s.StartsWith("IRequestHandler"))
+                .SelectMany(scope.GetElementsByShortName)
+                .FirstOrDefault(e => e is IInterface) as ITypeElement;
+
+            if (requestHandlerTypeElement is null)
             {
-                var scope = psiModule.GetPsiServices().Symbols.GetSymbolScope(
-                    psiModule,
-                    true,
-                    true
-                );
-
-                var requestHandlerType = scope
-                    .GetAllShortNames()
-                    .Where(s => s.StartsWith("IRequestHandler"))
-                    .SelectMany(scope.GetElementsByShortName)
-                    .FirstOrDefault(e => e is IInterface) as ITypeElement;
-
-                if (requestHandlerType is null)
-                {
-                    continue;
-                }
-
-                var declaredType = TypeFactory.CreateType(requestHandlerType);
-                
-                var targets = element.GetPsiServices().SingleThreadedFinder.FindAllInheritors(declaredType);
-
-                var declaredElements = targets.Select(target => target.GetTypeElement())
-                    .Where(e => e is not null)
-                    .SelectMany(e => e.Methods.Where(x => x.ShortName.StartsWith("Handle")))
-                    .ToArray();
-
-                return new NavigateTargets(declaredElements, false);
+                return base.CreateSearchRequest(context, element, initialTarget);
             }
+
+            var requestHandlerInterfaceType = TypeFactory.CreateType(requestHandlerTypeElement);
+
+            var requestHandlerImplementations = declaredElement.GetPsiServices().SingleThreadedFinder
+                .FindAllInheritors(requestHandlerInterfaceType);
+
+            var commandResponseType = initialTarget
+                .Substitution[(declaredElement as IMethod).TypeParameters.First()].GetScalarType()?.GetTypeElement();
+
+            if (commandResponseType is null)
+            {
+                return base.CreateSearchRequest(context, element, initialTarget);
+            }
+
+            var resultDeclaredElement = requestHandlerImplementations.Select(target => target.GetTypeElement())
+                .Where(e => e is not null)
+                .SelectMany(e =>
+                    e.Methods
+                    .Where(x =>
+                        x.ShortName == "Handle"
+                        && x.ReturnType.IsGenericTask(out var responseType)
+                        && commandResponseType.Equals(responseType.GetTypeElement())
+                    )
+                )
+                .FirstOrDefault();
+
+            if (resultDeclaredElement is null)
+            {
+                return base.CreateSearchRequest(context, element, initialTarget);
+            }
+
+            var searchDomain = SearchDomainContextUtil.GetSearchDomainContext(context).GetDefaultDomain().SearchDomain;
+
+            return new SearchImplementationsRequest(resultDeclaredElement, searchDomain);
         }
 
-        return NavigateTargets.Empty;
+        return base.CreateSearchRequest(context, element, initialTarget);
     }
 }
+
+// [PsiComponent(Instantiation.DemandAnyThreadSafe)]
+// public class MediatRCommandHandlerSearcherFactory : DomainSpecificSearcherFactoryBase
+// {
+//     public override bool IsCompatibleWithLanguage(PsiLanguageType languageType)
+//     {
+//         return languageType is CSharpLanguage;
+//     }
+//
+//     public override DerivedFindRequest GetDerivedFindRequest(IFindResultReference result)
+//     {
+//         var ex = result;
+//         var reference = result.Reference;
+//         var names = reference.GetAllNames();
+//
+//         return new DerivedFindRequest([result.DeclaredElement], x => true, true);
+//         return base.GetDerivedFindRequest(result);
+//     }
+//
+//     // public override NavigateTargets GetNavigateToTargets(IDeclaredElement element)
+//     // {
+//     //     if (element is IMethod method
+//     //         && method.ShortName == "Send"
+//     //         && method.TypeParametersCount == 1
+//     //         && method.ContainingType is IInterface @interface
+//     //         && @interface.GetContainingNamespace().ShortName == "MediatR"
+//     //         && method.Parameters.Count >= 1
+//     //         && method.Parameters[0].Type.GetTypeElement() is IInterface parameterInterface
+//     //         && parameterInterface.ShortName == "IRequest"
+//     //         && parameterInterface.GetContainingNamespace().ShortName == "MediatR"
+//     //         && parameterInterface.TypeParametersCount == 1
+//     //        )
+//     //     {
+//     //         var psiModules = element.GetSolution().GetPsiServices().Modules.GetModules();
+//     //         
+//     //         foreach (var psiModule in psiModules)
+//     //         {
+//     //             var scope = psiModule.GetPsiServices().Symbols.GetSymbolScope(
+//     //                 psiModule,
+//     //                 true,
+//     //                 true
+//     //             );
+//     //
+//     //             var requestHandlerType = scope
+//     //                 .GetAllShortNames()
+//     //                 .Where(s => s.StartsWith("IRequestHandler"))
+//     //                 .SelectMany(scope.GetElementsByShortName)
+//     //                 .FirstOrDefault(e => e is IInterface) as ITypeElement;
+//     //
+//     //             if (requestHandlerType is null)
+//     //             {
+//     //                 continue;
+//     //             }
+//     //
+//     //             var declaredType = TypeFactory.CreateType(requestHandlerType);
+//     //             
+//     //             var targets = element.GetPsiServices().SingleThreadedFinder.FindAllInheritors(declaredType);
+//     //
+//     //             var declaredElements = targets.Select(target => target.GetTypeElement())
+//     //                 .Where(e => e is not null)
+//     //                 .SelectMany(e => e.Methods.Where(x => x.ShortName.StartsWith("Handle")))
+//     //                 .ToArray();
+//     //
+//     //             return new NavigateTargets(declaredElements, false);
+//     //         }
+//     //     }
+//     //
+//     //     return NavigateTargets.Empty;
+//     // }
+// }
